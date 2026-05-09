28a268dc94961c4f1b5a90f773f086114c6eaf84
Max <maxiezequiel2005@gmail.com>
2026-05-08 22:57:05 -0300

    added form for classes

--- crud_auth_original/classes/forms.py
+++ crud_auth_original/classes/forms.py
@@ -1,3 +1,14 @@
 from django.core import validators
 from django import forms
 from classes.models import Course, inscription
+# meow
+class CourseForm(forms.ModelForm):
+    model = Course
+    fields = ["name","description","teacher","starts_at","ends_at"]
+    labels = {
+        "name":"Nombre de la clase",
+        "description":"descripcion de la clase",
+        "starts_at": "fecha y hora de inicio",
+        "ends_at": "fecha y hora de finalizacion"
+    }
+    widgets = forms.DateTimeInput(attrs={"type":"datetime-local"})
