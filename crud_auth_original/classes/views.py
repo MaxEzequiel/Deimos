c670162d7cdeec682609d3494d40e8273250edb7
Max <maxiezequiel2005@gmail.com>
2026-05-16 08:57:10 -0300

    added list class and edit class

--- crud_auth_original/classes/views.py
+++ crud_auth_original/classes/views.py
@@ -6,10 +6,11 @@ from django.contrib.auth.decorators import login_required
 # Create your views here. 
 # Mineaqua
 
+#  class es lo que vera el user, en codigo se manejara como course
 @login_required
 def create_class(request):
     if request.method == "GET":
-        return render(request, "create_class.html",{"class_form" : CourseForm()})
+        return render(request, "create_class.html",{"course_form" : CourseForm()})
     else:
         current_class = CourseForm(request.POST)
         if current_class.is_valid():
@@ -18,5 +19,23 @@ def create_class(request):
             course_instance.save()
             return redirect("home")
         else:
-            return render(request, "create_class.html", {"class_form" : CourseForm})
+            return render(request, "create_class.html", {"course_form" : CourseForm})
 
+
+def list_class(request):
+    if request.method == "GET": 
+        courses = Course.objects.all()
+        return render(request, "list_class.html",{"courses": courses})
+
+
+def edit_class(request, course_id): 
+    course = Course.objects.get(id = course_id)
+    if request.method == "GET":
+        form = CourseForm(instance=course)
+        return render(request, "edit_class.html", {"edit_form" : form})
+    else:
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect("list_class")
+        return render(request, "edit_class.html", {"edit_form" : form})
