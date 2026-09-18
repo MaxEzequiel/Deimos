5436c8222fd0da4202f64ff8563bbd0c16a213e0
Max <maxiezequiel2005@gmail.com>
2026-09-18 19:26:12 -0300

    fixed issue with password validator

--- crud_auth_original/accounts/password_validators.py
+++ crud_auth_original/accounts/password_validators.py
@@ -8,16 +8,16 @@ class UppercaseAndSpecialCharacterValidator:
         for character in password:
             if character.isupper():
                 has_uppercase = True
-            if not character.isalpha():
+            if not character.isalpha() and not character.isdigit():
                 has_special_character = True
 
         errors = []
         if not has_uppercase:
             errors.append("la contraseña debe tener al menos una letra mayuscula")
         if not has_special_character:
-            errors.append("la contraseña debe tener al menos un caracter especial o un numero")
+            errors.append("la contraseña debe tener al menos un caracter especial")
         if errors:
             raise ValidationError(errors)
 
     def get_help_text(self):
-        return "la contraseña debe tener al menos una letra mayuscula y un caracter especial o un numero"
+        return "la contraseña debe tener al menos una letra mayuscula y un caracter especial"
